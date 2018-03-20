da30a2b8b9ec9163044c55769cc65be1c9c4f595
Mateus Riva <mriva@ime.usp.br>
2018-03-19 21:55:00 -0300

    zapinating. need magic love on wordlists

---
+++ wordlists.py
@@ -0,0 +1,10 @@
+"""Contains special words which are specially zapinated, or not."""
+
+discard_tokens = ["que", "de"]
+"""Tokens that shall never be zapinated. NEVER."""
+
+specific_tokens = {"top" : ["👌"]}
+"""Wordlist of special tokens"""
+
+mood_emoji = {"happy": ["😀","😁","😂","😃","😄","😅","😆","😉","😊","😋","😎","☺","😛","😜","😝", "👌"]}
+"""list of emojis by mood"""
