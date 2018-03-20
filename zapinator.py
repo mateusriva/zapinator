ce4b3660e2133881c541312598a04fe443c8c48b
Mateus Riva <mriva@ime.usp.br>
2018-03-19 21:16:50 -0300

    basic encoding and test

--- zapinator.py
+++ zapinator.py
@@ -1,4 +1,6 @@
-"""Module for coming with zap baby"""
+"""Module for coming with zap baby
+
+We're gonna need this http://unicode.org/emoji/charts/full-emoji-list.html"""
 
 def zapinate(text, mood="happy"):
     """Function for zapinating a text.
@@ -13,9 +15,9 @@ def zapinate(text, mood="happy"):
     Returns
     -------
     zapinated_text : str
-        Text after zapination, Unicode-encoded.     
+        Text after zapination, <?>-encoded.     
     """
-    return text
+    return text + " 😀"
 
 if __name__ == '__main__':
     # main function, for debugging. modify for release.
@@ -23,5 +25,6 @@ if __name__ == '__main__':
     zapinated_text = zapinate(text)
 
     # save to simple html
-    out_file = open("out.html", "w")
+    out_file = open("out.html", "w", encoding="utf-8")
+    out_file.write("<head><meta charset='utf-8'/></head>\n")
     out_file.write("<body>{}</body>".format(zapinated_text))
