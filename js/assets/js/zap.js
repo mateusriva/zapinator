3a2836b0cfefcd1bd70e2880e3c5e8f5616fec9e
Vinicius Araujo <vinicius.araujo@arquivei.com.br>
2018-03-21 00:24:47 -0300

    adds more info to the main page

--- js/assets/js/zap.js
+++ js/assets/js/zap.js
@@ -1,5 +1,38 @@
-$(document).ready(function () {
+$(function() {
   $("#strength-slider").on("input change", e => {
-    $("#strength-show").html(parseInt(e.target.value / 100))
+    const zapStrength = e.target.value
+    
+    const emojiArray = [
+      "crazy",
+      "surprised",
+      "sunglasses",
+      "muscle",
+      "lol",
+      "okay",
+      "okay",
+      "top",
+    ]
+    const zapEmojisPerStrength = [1, 2, 3, 4, 8]
+    const strengthTitleArray = [
+      "Zap força fraca",
+      "Zap força média",
+      "Isso que é zap bb",
+      "Zap monstrão",
+      "Zap topper d+"
+    ]
+
+    const finalZap = `
+      <div class="mb10">${strengthTitleArray[zapStrength]}</div>
+      ${emojiArray.slice(0, zapEmojisPerStrength[zapStrength]).map(
+        emoji => `<img class="emoji" src="images/emojis/${emoji}.png" alt="${emoji}"/>`
+      ).join(" ")}
+    `
+
+    $("#strength-show").html(finalZap)
+  })
+
+  $(".mood-button").on("click", function() {
+    $(".mood-button").removeClass("active")
+    $(this).addClass("active")
   })
 })
