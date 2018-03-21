6c78dea8b1275497855b15a02ba287c704c6246d
Vinicius Araujo <vinicius.araujo@arquivei.com.br>
2018-03-21 01:32:19 -0300

    adds interactive zap strength
    now the emojis shown react to the selected mood

--- web/assets/js/zap.js
+++ web/assets/js/zap.js
@@ -1,8 +1,17 @@
-$(function() {
-  $("#strength-slider").on("input change", e => {
-    const zapStrength = e.target.value
-    
-    const emojiArray = [
+const zapStrengthShow = value => {
+  const zapEmojisPerStrength = [1, 2, 3, 4, 8]
+  const emojiArray = {
+    angry: [
+      "steaming",
+      "skullandbones",
+      "angry",
+      "stop",
+      "under18",
+      "bomb",
+      "bomb",
+      "cursing",
+    ],
+    happy: [
       "crazy",
       "surprised",
       "sunglasses",
@@ -11,28 +20,34 @@ $(function() {
       "okay",
       "okay",
       "top",
-    ]
-    const zapEmojisPerStrength = [1, 2, 3, 4, 8]
-    const strengthTitleArray = [
-      "Zap força fraca",
-      "Zap força média",
-      "Isso que é zap bb",
-      "Zap monstrão",
-      "Zap topper d+"
-    ]
+    ],
+  }
+  const strengthTitleArray = [
+    "Zap força fraca",
+    "Zap força média",
+    "Isso que é zap bb",
+    "Zap monstrão",
+    "Zap topper d+"
+  ]
+  const mood = $(".mood-button.active").data("mood")
+  const finalZap = `
+    <div class="mb10">${strengthTitleArray[value]}</div>
+    ${emojiArray[mood].slice(0, zapEmojisPerStrength[value]).map(
+      emoji => `<img class="emoji" src="images/emojis/${emoji}.png" alt="${emoji}"/>`
+    ).join(" ")}
+  `
 
-    const finalZap = `
-      <div class="mb10">${strengthTitleArray[zapStrength]}</div>
-      ${emojiArray.slice(0, zapEmojisPerStrength[zapStrength]).map(
-        emoji => `<img class="emoji" src="images/emojis/${emoji}.png" alt="${emoji}"/>`
-      ).join(" ")}
-    `
+  $("#strength-show").html(finalZap)
+}
 
-    $("#strength-show").html(finalZap)
+$(function() {
+  $("#strength-slider").on("input change", e => {
+    zapStrengthShow(e.target.value)
   })
 
   $(".mood-button").on("click", function() {
     $(".mood-button").removeClass("active")
     $(this).addClass("active")
+    zapStrengthShow($("#strength-slider").val())
   })
 })
